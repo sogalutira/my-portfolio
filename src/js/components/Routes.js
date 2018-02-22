33ba726b46c2c592c7133ea7b0cdb47f2b0bdacb
sgalutira <sheena.galutira@gmail.com>
2018-02-21 17:03:43 -1000

    added Switch

--- src/js/components/Routes.js
+++ src/js/components/Routes.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Portfolio from '../containers/Portfolio';
 import MenuLayout from '../containers/MenuLayout';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
+import { Switch } from 'react-router-dom';
 
 class Routes extends Component {
 	render(){
 		return (
 			<Router history={browserHistory}>
-				<Route 
-					exact path="/" 
-					component={MenuLayout}>
-				</Route>
-				<Route 
-					path="/portfolio" 
-					component={Portfolio}>
-				</Route>
+				<Switch>
+					<Route 
+						exact path="/" 
+						component={MenuLayout}>
+					</Route>
+					<Route 
+						path="/portfolio" 
+						component={Portfolio}>
+					</Route>
+				</Switch>
 			</Router>
 		)
 	}
