5abac6b1a3f223a3a9bdeb449245a7358d0741f0
sgalutira <sheena.galutira@gmail.com>
2018-02-27 21:06:03 -1000

    changed hover functions to css

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -16,46 +16,11 @@ class Hero extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isHovered: false,
-      show: "notActive",
-      isVis: false,
       isContactVisible: false
     };
-    this.handleHover = this.handleHover.bind(this);
-    this.toggleHoverState = this.toggleHoverState.bind(this);
-    this.addClassName = this.addClassName.bind(this);
-    this.removeClassName = this.removeClassName.bind(this);
-    this.handleVis = this.handleVis.bind(this);
+
     this.handleContact = this.handleContact.bind(this);
     this.isHome = this.isHome.bind(this);
-  }
-
-  addClassName() {
-    this.setState({show: "isActive"});
-  }
-
-  removeClassName() {
-    this.setState({show: "notActive"});
-  }
-
-  handleHover() {
-    this.setState(this.toggleHoverState);
-  }
-
-  toggleHoverState(state) {
-    return {
-      isHovered: !this.state.isHovered,
-    };
-  }
-
- handleVis() {
-    this.setState(this.toggleVisState);
-  }
-
-  toggleVisState(state) {
-    return {
-      isVis: !this.state.isVis,
-    };
   }
 
   isHome() {
@@ -67,8 +32,6 @@ class Hero extends Component {
   }
 
 	render() {
-    const willChange = this.state.isHovered ? 'isActive': 'notActive';
-    const willShow = this.state.isVis ? 'isActive': 'notActive';
     const blur = this.state.isContactVisible ? 'blur-eff': '';
 		return (
       <div>
@@ -82,11 +45,8 @@ class Hero extends Component {
             </div>
             <div id="holder">
             <div className="title-selection">
-              <div className="icon-holder"
-                onMouseEnter={this.handleHover}
-                onMouseLeave={this.handleHover}
-              >
-                <div id="home-plane" className={willChange}>
+              <div className="icon-holder">
+                <div id="home-plane" className="show-on-hover">
                   <i className="far fa-paper-plane"></i>
                   </div>
                 <div className="spacer"></div>
@@ -94,19 +54,17 @@ class Hero extends Component {
                   <Link to="/" replace>Home</Link>
                 </div>
               </div>
-              <div className="icon-holder"
-                onMouseEnter={this.addClassName}
-                onMouseLeave={this.removeClassName}
-              >
-                <div id="port-plane" className={this.state.show}><i className="far fa-paper-plane"></i></div>
+              <div className="icon-holder">
+                <div id="port-plane" className="show-on-hover">
+                  <i className="far fa-paper-plane"></i>
+                </div>
                 <div className="spacer"></div>
                 <div><Link to="/portfolio">Portfolio</Link></div>
               </div>
-              <div className="icon-holder"
-                onMouseEnter={this.handleVis}
-                onMouseLeave={this.handleVis}
-              >
-                <div id="contact-plane" className={willShow}><i className="far fa-paper-plane"></i></div>
+              <div className="icon-holder">
+                <div id="contact-plane" className="show-on-hover">
+                  <i className="far fa-paper-plane"></i>
+                </div>
                 <div className="spacer"></div>
                 <div id="contact" onClick={this.handleContact}>Contact</div>
               </div>
